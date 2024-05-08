a250a2447f1b65f783ecc3b3642132b334ffaf4f
SuiYunsy <104718844+SuiYunsy@users.noreply.github.com>
2024-05-08 16:35:52 +0800

    Update Model Lists

--- app/constant.ts
+++ app/constant.ts
@@ -135,32 +135,23 @@ export const KnowledgeCutOffDate: Record<string, string> = {
 };
 
 const openaiModels = [
+  "deepseek-chat",
   "gpt-3.5-turbo",
-  "gpt-3.5-turbo-1106",
   "gpt-3.5-turbo-0125",
   "gpt-4",
-  "gpt-4-0613",
-  "gpt-4-32k",
-  "gpt-4-32k-0613",
-  "gpt-4-turbo",
-  "gpt-4-turbo-preview",
+  "gpt-4-0125-preview",
   "gpt-4-vision-preview",
-  "gpt-4-turbo-2024-04-09",
+  "gpt-4-turbo",
 ];
 
 const googleModels = [
-  "gemini-1.0-pro",
-  "gemini-1.5-pro-latest",
+  "gemini-pro",
   "gemini-pro-vision",
+  "gemini-1.5-pro-latest",
 ];
 
 const anthropicModels = [
-  "claude-instant-1.2",
-  "claude-2.0",
-  "claude-2.1",
-  "claude-3-sonnet-20240229",
-  "claude-3-opus-20240229",
-  "claude-3-haiku-20240307",
+  "claude",
 ];
 
 export const DEFAULT_MODELS = [
