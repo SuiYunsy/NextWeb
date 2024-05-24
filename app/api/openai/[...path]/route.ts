88dd5ee77a1d2d0ad9eaf6b91da08f798c1a66ad
SuiYunsy <104718844+SuiYunsy@users.noreply.github.com>
2024-05-24 15:30:41 +0800

    should use edge

--- app/api/openai/[...path]/route.ts
+++ app/api/openai/[...path]/route.ts
@@ -73,24 +73,24 @@ async function handle(
 
 export const GET = handle;
 export const POST = handle;
-export const maxDuration = 60;
-export const runtime = "nodejs";
+// export const maxDuration = 60;
+export const runtime = "edge";
 export const preferredRegion = [
-  "arn1",
-  "bom1",
-  "cdg1",
-  "cle1",
-  "cpt1",
-  "dub1",
+  // "arn1",
+  // "bom1",
+  // "cdg1",
+  // "cle1",
+  // "cpt1",
+  // "dub1",
   "fra1",
-  "gru1",
-  "hnd1",
-  "iad1",
-  "icn1",
-  "kix1",
-  "lhr1",
-  "pdx1",
-  "sfo1",
-  "sin1",
-  "syd1",
+  // "gru1",
+  // "hnd1",
+  // "iad1",
+  // "icn1",
+  // "kix1",
+  // "lhr1",
+  // "pdx1",
+  // "sfo1",
+  // "sin1",
+  // "syd1",
 ];
